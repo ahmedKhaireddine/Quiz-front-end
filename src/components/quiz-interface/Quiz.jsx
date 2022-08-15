2316dffbaa00041e3229afbaa9d19b5a0d0f90c1
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2022-08-15 18:20:36 +0200

    Create component that displays the questions

---
+++ src/components/quiz-interface/Quiz.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+import Questions from "../../questions.json"
+import ChoiceList from "./ChoiceList"
+
+const Quiz = () => {
+  const [index, setIndex] = useState(0)
+  const { choices, title } = Questions[index]
+
+  return (
+    <div>
+      <h2>{index + 1}. {title}</h2>
+      <ChoiceList choices={choices}/>
+      <button onClick={() => setIndex(index + 1)}>Question suivant</button>
+    </div>
+  )
+}
+
+export default Quiz
