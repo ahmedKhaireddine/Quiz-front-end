a8ea5e379b8fd499d0978903906ae88f078a0b6a
kevinalbert7 <20ko@live.fr>
2022-08-17 20:24:09 +0200

    Installation du package formik et yup

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,23 +1,9 @@
-import React from 'react'
-import styled from 'styled-components'
 
-import Button from '../components/Button'
-import QuizLayout from '../components/layouts/QuizLayout'
-import Logo from '../components/Logo'
-
-const Card = styled.div`
-    height: 200px;
-    width: 200px;
-    background: salmon
-`
 
 const Test = () => {
     return (
-        <QuizLayout>
-            <Logo/>
-            <Button/>
-            <Card/>
-        </QuizLayout>
+        <div>
+        </div>
     )
 }
 
