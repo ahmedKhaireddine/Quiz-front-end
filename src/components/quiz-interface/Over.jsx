bdbd5fcd95fb7dae206bdf7cff7fd687e8502180
kevinalbert7 <20ko@live.fr>
2022-08-24 11:47:12 +0200

    modification de Over

--- src/components/quiz-interface/Over.jsx
+++ src/components/quiz-interface/Over.jsx
@@ -5,7 +5,7 @@ import React from "react"
 const Over = (props) => {
     return (
         <div>
-            
+            <p>Félicitations! Vous avez terminé le quiz!</p>
         </div>
     )
 }
