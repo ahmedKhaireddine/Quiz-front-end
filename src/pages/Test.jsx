2ebb2820dc4d44bec98705529266319bbc5ca4d8
kevinalbert7 <20ko@live.fr>
2022-08-24 18:10:47 +0200

    création fonction nextQuestion

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -17,13 +17,10 @@ const Test = (props) => {
     return (
         <div>
             {/* <PlayerInfos/> */}
-<<<<<<< HEAD
             <Quiz />
             {/* <Over /> */}
             {/* <ChoiceList /> */}
-=======
             <Button />
->>>>>>> ddbacf9c6f9d328049b277530a07a470418f4b92
         </div>
     )
 }
