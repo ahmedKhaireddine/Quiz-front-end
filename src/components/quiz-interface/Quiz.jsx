a81b997cf2dd70772ec581da8d998c4e5deb07b6
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2022-08-26 10:09:45 +0100

    Create a useTimer hook and use it in the Quiz component

--- src/components/quiz-interface/Quiz.jsx
+++ src/components/quiz-interface/Quiz.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import Questions from "../../questions.json"
-import ChoiceList from "./core/Stepper"
+import ChoiceList from "./core/ChoiceList"
+import { useTimer } from "../../hooks/useTimer"
 
 const Quiz = () => {
   const [index, setIndex] = useState(0)
+  const { time, start } = useTimer({
+    onTimeOver: () => {
+      if(index < Questions.length) {
+        setIndex(index => index + 1)
+      }
+    },
+    order: "DECREMENTAL"
+  })
   const { choices, title } = Questions[index]
+
+  useEffect(() => {
+    start(Questions[index].duration)
+  }, [index, start])
 
   return (
     <div>
+      <h1>Timer {time}</h1>
       <h2>{index + 1}. {title}</h2>
       <ChoiceList choices={choices}/>
       <button onClick={() => setIndex(index + 1)}>Question suivant</button>
