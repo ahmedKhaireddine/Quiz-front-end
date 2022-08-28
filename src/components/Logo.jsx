be9c5fa4f40769caee745d13128c706a75576b32
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2022-08-28 10:04:59 +0100

    Improve the logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,28 +2,29 @@ import React from "react"
 import styled from "styled-components"
 
 const Span = styled.span`
-    font-family: 'Henny Penny';
+    font-family: "Henny Penny";
     font-size: ${props => props.size};
     color: #F1F1F1;
 `
-    
 const Link = styled.a`
     text-decoration: none;
+    margin: ${props => props.margin};
 `
-const Logo = ({href, size}) => {
+const Logo = ({ href, margin, size }) => {
     return (
-        <Link href={href}>
+        <Link href={href} margin={margin}>
             <Span size={size}>
-                Quiz
+                Quiz!!!
             </Span>
         </Link>
-        
+
     )
 }
 
 Logo.defaultProps = {
-    href: '#',
-    size: '100px'
+    href: "#",
+    margin: "0px",
+    size: "100px"
 }
 
 export default Logo
