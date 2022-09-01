58ac3f1965e1abb51244d5ade92e72101367d509
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2022-09-01 13:31:27 +0100

    Add base style over component

--- src/components/quiz-interface/Over.jsx
+++ src/components/quiz-interface/Over.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 
-import Title from "./Title"
+import { Item, MagicItems, Text } from "../../styles/OverStyled"
 
 const Over = (props) => {
     return (
-        <Title>
-            Félicitations! Vous avez terminé le quiz!
-        </Title>
+        <>
+            <Text size="3em">Félicitations!</Text>
+            <Text size="2em">Vous avez terminé le quiz!</Text>
+            <MagicItems>
+                {Array(8).fill().map((el, index) => <Item key={index}>✨</Item>)}
+            </MagicItems>
+        </>
     )
 }
 
