9cd5759015c7837f4a4a13fa7a175401538a0c50
kevinalbert7 <20ko@live.fr>
2022-09-18 22:02:08 +0200

    Animation du Logo

--- src/components/quiz-interface/Home.jsx
+++ src/components/quiz-interface/Home.jsx
@@ -10,6 +10,7 @@ import styled from "styled-components"
 import TextField from "../TextField"
 import Logo from "../Logo"
 import ButtonComponent from "./core/ButtonComponent"
+import LogoAnim from "../../others/LogoAnim"
 
 const Box = styled.div`
     display: flex;
@@ -37,7 +38,7 @@ const Home = () => {
         >
             {(formik) => ( 
                 <Box width={[1, 1 / 2, 1 / 4]} >
-                    <Logo />
+                    <LogoAnim />
                     <Heading
                         as='h1'
                         my="60px"    
---
+++ src/others/LogoAnim.jsx
@@ -0,0 +1,34 @@
+import "./LogoAnimStyle.css"
+
+const LogoAnim = () => {
+  return (
+    <div className="grid">
+        <div className="letter-anim">
+            <span data-letter="Q">Q</span>
+        </div>
+        <div className="letter-anim">
+            <span data-letter="U">U</span>
+        </div>
+        <div className="letter-anim">
+            <span data-letter="I">I</span>
+        </div>
+        <div className="letter-anim">
+            <span data-letter="Z">Z</span>
+        </div>
+        <div className="letter-anim">
+            <span data-letter="">&nbsp;</span>
+        </div>
+        <div className="letter-anim">
+            <span data-letter="!">!</span>
+        </div>
+        <div className="letter-anim">
+            <span data-letter="!">!</span>
+        </div>
+        <div className="letter-anim">
+            <span data-letter="!">!</span>
+        </div> 
+    </div>
+  )
+}
+
+export default LogoAnim
