a3f87c8256a146215e03d49ee77b64abf60d3c0f
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2022-10-23 10:48:46 +0200

    Improve login form using table of fields

--- src/components/signin/SignInFormik.jsx
+++ src/components/signin/SignInFormik.jsx
@@ -1,26 +1,28 @@
 import { useContext } from "react"
 import { Formik } from "formik"
-import { IoPersonSharp } from "react-icons/io5"
-import { RiLockFill } from "react-icons/ri"
 import { useNavigate } from "react-router-dom"
 
 import Button from "../quiz-interface/core/Button"
+import FieldsList from "../FieldsList"
 import { Form, Link, Text } from "../../styles/ReusableTagsStyled"
+import { LOGIN_FIELD_ATTRIBUTES } from "../../constants"
 import { SignInSchema } from "../../validations/auth"
-import TextFieldWithIcon from "../TextFieldWithIcon"
 import { UserContext } from "../../contexts/User"
 
 const SignInFormik = (props) => {
   const { setUser } = useContext(UserContext)
   let navigate = useNavigate()
-  
-  const signIn = (values, actions) => {
+
+  const onSubmit = (values, actions) => {
     setUser({
+      active: "true",
       email: values.email,
       role:"user",
       token:"string",
     })
+
     navigate("/dashboard")
+
     actions.resetForm()
   }
 
@@ -30,26 +32,14 @@ const SignInFormik = (props) => {
         email: "",
         password: ""
       }}
-      onSubmit={signIn}
+      onSubmit={onSubmit}
       validationSchema={SignInSchema}
     >
       {(formik) => (
         <Form
           onSubmit={formik.handleSubmit}
         >
-          <TextFieldWithIcon
-            autoFocus
-            name="email"
-            placeholder="Email..."
-            type="email"
-            icon={<IoPersonSharp />}
-          />
-          <TextFieldWithIcon
-            name="password"
-            placeholder="Mot de passe..."
-            type="password"
-            icon={<RiLockFill/>}
-          />
+          <FieldsList data={LOGIN_FIELD_ATTRIBUTES}/>
           <Button
             boxShadow= "rgba(0, 0, 0, 0.35) 0px 5px 15px"
             margin="20px auto 30px"
