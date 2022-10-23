9da5473f8d66bd5c78cc80a54b22abc73ade0b7c
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2022-10-23 10:47:05 +0200

    Add fields table for sign in page

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,7 @@
 import ATTRIBUTES_REGISTEATION_FIELDS from "./attributes-registration-fields"
+import LOGIN_FIELD_ATTRIBUTES from "./login-field-attributes"
 
 export {
-  ATTRIBUTES_REGISTEATION_FIELDS
+  ATTRIBUTES_REGISTEATION_FIELDS,
+  LOGIN_FIELD_ATTRIBUTES
 }
